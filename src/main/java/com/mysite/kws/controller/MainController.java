a648849216049dc828cde626eec04f8ad49d9e90
sookyeong <sookyeong0505@gmail.com>
2024-04-26 09:57:06 +0900

    리액트 버전 추가

--- src/main/java/com/mysite/kws/controller/MainController.java
+++ src/main/java/com/mysite/kws/controller/MainController.java
@@ -1,14 +1,36 @@
 package com.mysite.kws.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
 public class MainController {
 
     @GetMapping("/")
-    public String main() {
-        return "redirect:/question/list";
+    public List<String> main() {
+        return Arrays.asList("안녕하세요", "Hello");
+    }
+
+    @PostMapping(value="/testData")
+    public Map<Integer, String> testData(@RequestBody List<String> params){
+        Map<Integer, String> data = new HashMap<>();
+        data.put(1,"사과");
+        data.put(2,"포도");
+        data.put(3,"바나나");
+
+        int i = 4;
+        for(String param : params){
+            data.put(i, param);
+            i++;
+        }
+
+        return data;
     }
 }
